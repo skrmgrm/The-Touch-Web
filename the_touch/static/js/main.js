4c8fd70e75999aa4b687799a370bd5929d347751
Joshua Barlolong <joshuabarlolong@gmail.com>
2021-07-30 17:04:30 +0800

    added sidebar incomplete with bugs

--- the_touch/static/js/main.js
+++ the_touch/static/js/main.js
@@ -6,16 +6,14 @@ const nav = document.querySelector(".nav");
 const socials = document.querySelector(".socials");
 
 menu.addEventListener("click", () => {
-    nav.classList.toggle("active");
-    socials.classList.toggle("active");
+  nav.classList.toggle("active");
+  socials.classList.toggle("active");
 
-    if (nav.classList.contains("active")) {
-        menuIcon.classList.add("fa-times");
-        menuIcon.classList.remove("fa-bars");
-    } else {
-        menuIcon.classList.remove("fa-times");
-        menuIcon.classList.add("fa-bars");
-    }
+  if (nav.classList.contains("active")) {
+    menuIcon.classList.add("fa-times");
+    menuIcon.classList.remove("fa-bars");
+  } else {
+    menuIcon.classList.remove("fa-times");
+    menuIcon.classList.add("fa-bars");
+  }
 });
-
-console.log("Hello World");
